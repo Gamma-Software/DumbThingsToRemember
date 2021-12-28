32a6f236c13c31d8de6e8c4fbb2c2689443a5534
valentin.rudloff <valentin.rudloff.perso@gmail.com>
2021-12-28 20:59:46 +0100

    update script to build deb

---
+++ build_deb/build_deb.sh
@@ -0,0 +1,28 @@
+# Configure your paths and filenames
+SOURCEBINPATH=/usr/bin
+SOURCEBIN=add_dumb_things.sh
+DEBFOLDER=dumb-things
+DEBVERSION=1.0
+DEBEMAIL="valentin.rudloff.perso@gmail.com"
+DEBFULLNAME="Valentin RUDLOFF"
+REVISION=1
+ARCHITECTURE=all
+
+DEBFOLDERNAME=${DEBFOLDER}_${DEBVERSION}-${REVISION}_${ARCHITECTURE}
+
+# Create your scripts source dir
+rm -rf $DEBFOLDERNAME # Make sure it's clean
+mkdir $DEBFOLDERNAME
+
+# Copy your script to the user bin dir
+mkdir -p $DEBFOLDERNAME/$SOURCEBINPATH
+cp ../$SOURCEBIN $DEBFOLDERNAME/$SOURCEBINPATH
+
+# Copy DEBIAN folder
+cp -r DEBIAN $DEBFOLDERNAME
+
+# Copy changelog
+cp ../CHANGELOG.md $DEBFOLDERNAME/DEBIAN/changelog
+
+# Build deb package
+dpkg-deb --build --root-owner-group $DEBFOLDERNAME
